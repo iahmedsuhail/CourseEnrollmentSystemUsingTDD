dec71fc4ec0764a04121d3404c216af3847cf060
Ahmed Suhail <iahmedsuhail@gmail.com>
2020-05-13 21:15:45 +1200

    Add Tests for Course Details to be seen
    The tests won't compile because the class methods don't exist.

---
+++ src/test/CourseTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+public class CourseTest {
+
+    @org.junit.Test
+    public void testCourseDetailsCanBeSeen() {
+        Course course = new Course();
+        assertEquals("Software Requirements Engineering", course.getName());
+        assertEquals("SOFTENG754", course.getCode());
+        assertEquals(15, course.getPoints());
+        assertEquals(true, course.getAvailaibility());
+    }
+}
