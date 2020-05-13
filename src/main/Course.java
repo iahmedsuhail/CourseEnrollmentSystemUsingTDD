f8b60a9853f445100924fb5aa0dead02c96c4d6b
Ahmed Suhail <iahmedsuhail@gmail.com>
2020-05-14 11:10:08 +1200

    Tests Now Pass
    - Some tests were re-written as deemed necessary
    - All tests were made to pass

--- src/main/Course.java
+++ src/main/Course.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
+
 public class Course {
     String name;
     String code;
     int points;
     boolean availaibility;
 
+    int maxNumberOfStudents;
+    ArrayList<String> prereqs;
+
     public Course(String name, String code, Integer points, boolean availaibility) {
         this.name = name;
         this.code = code;
         this.points = points;
         this.availaibility = availaibility;
+        this.prereqs = new ArrayList<>();
     }
 
     public String getName() {
@@ -25,5 +31,25 @@ public class Course {
 
     public boolean getAvailaibility() {
         return availaibility;
+    }
+
+    public void setMaxNumberOfStudents(int maxNumberOfStudents) {
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+
+    public void addPrereqs(String code) {
+        this.prereqs.add(code);
+    }
+
+    public void setAvailaibility(boolean availaibility) {
+        this.availaibility = availaibility;
+    }
+
+    public ArrayList<String> getPrereqs() {
+        return prereqs;
+    }
+
+    public int getMaxNumberOfStudents() {
+        return maxNumberOfStudents;
     }
 }
--- src/test/AdminTest.java
+++ src/test/AdminTest.java
@@ -4,21 +4,22 @@ import static org.junit.Assert.*;
 
 public class AdminTest {
 
-    @org.junit.Before
-    public void createCourseObject() {
-        Course course = new Course("Software Requirements Engineering", "SOFTENG754", 15, true);
-    }
-    
+
     @org.junit.Test
     public void testAdminCanSetGuidelines() {
+        Course course = new Course("Software Requirements Engineering", "SOFTENG754", 15, true);
 //        setting maximum number of students that can get into a course
-        assertEquals(50, course.setMaxNumberOfStudents(50));
+        course.setMaxNumberOfStudents(50);
+        assertEquals(50, course.getMaxNumberOfStudents());
 
 //        set the prerequisites for a course
-        ArrayList<Course> resultPrereqs = new ArrayList<Course>("SOFTENG251");
-        assertEquals(resultPrereqs, course.setPrereqs("SOFTENG251"));
+        course.addPrereqs("SOFTENG251");
+        ArrayList<String> resultPrereqs = new ArrayList<>();
+        resultPrereqs.add("SOFTENG251");
+        assertEquals(resultPrereqs, course.getPrereqs());
 
 //        set availability
-        assertEquals(false, course.setAvailaibility(false));
+        course.setAvailaibility(false);
+        assertEquals(false, course.getAvailaibility());
     }
 }
