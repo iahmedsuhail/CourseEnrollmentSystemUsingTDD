4b70eddb8beacf1bd9ca174b8217723675ef2f39
Ahmed Suhail <iahmedsuhail@gmail.com>
2020-05-13 21:21:43 +1200

    Add functionality to make test case pass
    Course Details can now be seen

--- src/main/Course.java
+++ src/main/Course.java
@@ -1,2 +1,29 @@
 public class Course {
+    String name;
+    String code;
+    int points;
+    boolean availaibility;
+
+    public Course(String name, String code, Integer points, boolean availaibility) {
+        this.name = name;
+        this.code = code;
+        this.points = points;
+        this.availaibility = availaibility;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public boolean getAvailaibility() {
+        return availaibility;
+    }
 }
--- src/test/CourseTest.java
+++ src/test/CourseTest.java
@@ -4,7 +4,7 @@ public class CourseTest {
 
     @org.junit.Test
     public void testCourseDetailsCanBeSeen() {
-        Course course = new Course();
+        Course course = new Course("Software Requirements Engineering", "SOFTENG754", 15, true);
         assertEquals("Software Requirements Engineering", course.getName());
         assertEquals("SOFTENG754", course.getCode());
         assertEquals(15, course.getPoints());
