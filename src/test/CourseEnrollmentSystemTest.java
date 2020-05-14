1742504cbdebbe877e8d997e8047802e95aa2f10
vaibhav <vaibhavnrw@gmail.com>
2020-05-14 18:02:18 +1200

    Update CourseEnrollmentSystemTest.java

--- src/test/CourseEnrollmentSystemTest.java
+++ src/test/CourseEnrollmentSystemTest.java
@@ -1,5 +1,12 @@
 import static org.junit.Assert.*;
 
 public class CourseEnrollmentSystemTest {
+    @org.junit.Test
+    public void GetEnrollmentStatus()
+    {
+        Student student=new Student();
+        boolean output=student.getEnrollmentStatus()'
+        assertTrue(output);
+    }
 
 }
