6d758a847e4bc69e719bfc17401db206cb7c7ac4
Ahmed Suhail <iahmedsuhail@gmail.com>
2020-05-14 16:07:10 +1200

    Add missing call to method for test case

--- src/test/ControllerTest.java
+++ src/test/ControllerTest.java
@@ -5,6 +5,7 @@ public class ControllerTest {
     @org.junit.Test
     public void testPersonCanLogin() {
         Person person = new Person();
+        person.login("username", "password");
         assertTrue(person.isLoggedIn());
     }
 }
