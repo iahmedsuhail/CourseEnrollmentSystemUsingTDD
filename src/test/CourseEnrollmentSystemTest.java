7473a5e2802e0c731c075e1b135ad9c68cf20e1b
Ahmed Suhail <iahmedsuhail@gmail.com>
2020-05-15 13:13:21 +1200

    Revert "Update CourseEnrollmentSystemTest.java"
    This reverts commit a43ee91e78d5a2d86d69e50d3c77bcee56e31ddb.

--- src/test/CourseEnrollmentSystemTest.java
+++ src/test/CourseEnrollmentSystemTest.java
@@ -1,12 +1,5 @@
 import static org.junit.Assert.*;
 
 public class CourseEnrollmentSystemTest {
-    @org.junit.Test
-    public void GetEnrollmentStatus()
-    {
-        Student student=new Student();
-        boolean output=student.getEnrollmentStatus()'
-        assertTrue(output);
-    }
 
 }
