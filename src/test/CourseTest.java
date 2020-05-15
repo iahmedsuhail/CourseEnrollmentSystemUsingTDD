6766fcc139c412c6d955c09e615e2f9a437cf16d
Ahmed Suhail <iahmedsuhail@gmail.com>
2020-05-15 13:50:16 +1200

    Add already passing test for the user story.
    After writing the test case, I got to know that the required functionality was already present in the code.
    
    Adding the commit so I can talk about this in the assignment report.

--- src/test/CourseTest.java
+++ src/test/CourseTest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class CourseTest {
@@ -9,5 +10,28 @@ public class CourseTest {
         assertEquals("SOFTENG754", course.getCode());
         assertEquals(15, course.getPoints());
         assertEquals(true, course.getAvailability());
+    }
+
+    @org.junit.Test
+    public void Given_StudentIsEnrolledInACourse_Expect_StudentCanSeeTheEnrolledCourses() {
+        Student student = new Student();
+
+//        Create 3 new courses
+        Course course = new Course("Software Requirements Engineering", "SOFTENG754", 15, true);
+        Course course1 = new Course("High Performance Computing", "SOFTENG751", 15, true);
+        Course course2 = new Course("Software Tools and Techniques", "SOFTENG750", 15, true);
+
+//        Enroll the student into the courses
+        student.enrol(course);
+        student.enrol(course1);
+        student.enrol(course2);
+
+//        Result object for comparison
+        ArrayList<Course> resultEnrolledCourses = new ArrayList<>();
+        resultEnrolledCourses.add(course);
+        resultEnrolledCourses.add(course1);
+        resultEnrolledCourses.add(course2);
+
+        assertEquals(resultEnrolledCourses, student.getEnrolledCourses());
     }
 }
