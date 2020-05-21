82a5d64abfd5b87191b977f548f981ba5305b0be
kali435 <61809500+kali435@users.noreply.github.com>
2020-05-21 23:58:23 +1200

    Time Table Planning Test

---
+++ src/test/java/TimeTablePlanning.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TimeTablePlanning {
+
+    @Test
+    public void testGetTimeTable(){
+        EnrollmentManager system = new EnrollmentManager();
+        TimeTable tb = new TimeTable();
+        CourseClass courseClass = new CourseClass("Software Requirements Engineering", "SOFTENG754", 15, true);
+        TimeTableEntry entry = new TimeTableEntry((new Date()),new Classes("102", courseClass), courseClass);
+        tb.addToTimeTable(entry);
+            }
+
+}
