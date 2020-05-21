55d50d2db65f77db30291bce1647f75f4140070d
Ahmed Suhail <iahmedsuhail@gmail.com>
2020-05-21 13:12:18 +1200

    Tests for submitting description with concession now compile
    Method stubs now exist in the code.
    
    Travis build should still fail

--- src/main/java/EnrollmentConcession.java
+++ src/main/java/EnrollmentConcession.java
@@ -1,15 +1,22 @@
-
-
 public class EnrollmentConcession {
     Student student;
     Course course;
+    String description;
 
     public EnrollmentConcession(Student student, Course course) {
         this.student = student;
         this.course = course;
     }
 
+    public EnrollmentConcession(Student student, Course course, String description){
+
+    }
+
     public String getDetails() {
         return String.format("%s wants to enrol in %s", student.getName(), course.getCode());
+    }
+
+    public String getDescription(){
+        return "";
     }
 }
