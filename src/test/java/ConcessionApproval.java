ba0bb09690d4f5eba4656545a20d3b560876f6e1
kali435 <61809500+kali435@users.noreply.github.com>
2020-05-21 23:52:01 +1200

    ConcessionApproval test

---
+++ src/test/java/ConcessionApproval.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConcessionApproval {
+    AdminClass adminClass;
+    CourseClass courseClass;
+
+    @Before
+    public void init() {
+        adminClass = new AdminClass();
+        courseClass = new CourseClass("Software Requirements Engineering", "SOFTENG754", 15, true);
+    }
+
+    @Test
+    public void testCheckEnrollmentConcessions() {
+        
+        stu.setName("testUser");
+        EnrollmentConcession con = new EnrollmentConcession(stu, courseClass);
+        adminClass.acceptConscession(con);
+        assertEquals(true, con.getAccepted());
+        adminClass.rejectConscession(con);
+        assertEquals(false, con.getAccepted());
+    }
+}
