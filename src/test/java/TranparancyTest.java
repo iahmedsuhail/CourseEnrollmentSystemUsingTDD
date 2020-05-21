ccccf02bc997d8916e452b0a237011a64ffba49d
kali435 <61809500+kali435@users.noreply.github.com>
2020-05-21 23:00:45 +1200

    Transparency -1stCommit

---
+++ src/test/java/TranparancyTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class TranparancyTest {
+    StudentClass studentClass;
+    CourseClass courseClass;
+    @Before
+    public void init(){
+        studentClass = new StudentClass();
+        courseClass = new CourseClass("Software Requirements Engineering", "SOFTENG754", 15, true);
+
+    }
+    @Test(expected = IllegalStateException.class)
+    public void testConcessionsBeforeEnrollment(){
+        ;
+        studentClass.enrol(new CourseClass("test", "code",01,true));
+    }
+}
