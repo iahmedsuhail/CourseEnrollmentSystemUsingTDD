5e35a5d8b80ebee7ed7bd02ab48ffa2677a01768
kali435 <61809500+kali435@users.noreply.github.com>
2020-05-21 23:55:11 +1200

    Update Concession Approval Fix

--- src/test/java/ConcessionApproval.java
+++ src/test/java/ConcessionApproval.java
@@ -15,7 +15,7 @@ public class ConcessionApproval {
 
     @Test
     public void testCheckEnrollmentConcessions() {
-        
+        StudentClass stu = new StudentClass();
         stu.setName("testUser");
         EnrollmentConcession con = new EnrollmentConcession(stu, courseClass);
         adminClass.acceptConscession(con);
