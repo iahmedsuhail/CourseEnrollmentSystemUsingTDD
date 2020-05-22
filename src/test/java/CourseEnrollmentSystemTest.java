31f59be7dcda3e8a72e691fbd9e546bfc4c4c795
Ahmed Suhail <iahmedsuhail@gmail.com>
2020-05-22 17:03:40 +1200

    Add Integration Tests
    - Modules Student, EnrollmentConcession, Course, Teacher tested.

--- src/test/java/CourseEnrollmentSystemTest.java
+++ src/test/java/CourseEnrollmentSystemTest.java
@@ -1,5 +1,25 @@
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import java.util.ArrayList;
 import static org.junit.Assert.*;
+
+interface IntegrationTests { /* Category marker */ }
 
 public class CourseEnrollmentSystemTest {
 
+    @Category(IntegrationTests.class)
+    @org.junit.Test
+    public void test_Student_CanSubmit_EnrollmentConcession_ForA_Course_WithA_Teacher(){
+        Student student = new Student();
+        Teacher teacher = new Teacher("Kelly Blincoe");
+        Course course = new Course("Software Requirements Engineering",
+                "SOFTENG754", 15, true, teacher, 500);
+
+        student.enrol(course);
+
+        ArrayList<Course> resultEnrolledCourses = new ArrayList<>();
+        resultEnrolledCourses.add(course);
+
+        assertEquals(resultEnrolledCourses, student.getEnrolledCourses());
+    }
 }
